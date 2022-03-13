e22d33623cb27726b8c056e85a6999b6a148cf15
dyggod <dyggod@163.com>
2022-03-13 17:05:11 +0800

    添加基础云函数调用

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue';
+import { createUI } from 'taro-ui-vue3';
+import Taro from '@tarojs/taro';
+
 import './app.less';
+
+import 'taro-ui-vue3/dist/style/index.scss';
+
+Taro.cloud.init({
+  env: 'web-test-01-oq644',
+});
 
 const App = createApp({
   onShow(options) {
@@ -7,5 +16,9 @@ const App = createApp({
   },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 });
+
+// 引用全部组件
+const tuv3 = createUI();
+App.use(tuv3);
 
 export default App;
--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -3,12 +3,29 @@
     <text>
       {{ text }}
     </text>
+    <at-button
+      type="primary"
+      @click="addRecord"
+    >
+      点击上传动态
+    </at-button>
   </view>
 </template>
 <script lang="ts">
 import {
   reactive, toRefs, onBeforeMount, onMounted, ref,
 } from 'vue';
+import Taro from '@tarojs/taro';
+
+function addRecord() {
+  Taro.cloud.callFunction({
+    name: 'yy_addRecord',
+  }).then((res) => {
+    console.log('res: ', res);
+  }).catch((err) => {
+    console.log('err: ', err);
+  });
+}
 </script>
 <script lang='ts' setup>
 const text = ref('你好');
